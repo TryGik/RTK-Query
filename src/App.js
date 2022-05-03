bc101b25d9a74e63a2a998e4df772b3edbb149ce
TryGik <ahutinon@gmail.com>
2022-05-03 15:33:53 +0300

    mutation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import { useState } from 'react';
 import './App.css';
-import { useGetGoodsQuery } from './redux';
+import { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation } from './redux';
 
 function App() {
-  const [count, setCount] = useState();
-  const { data = [], isLoading } = useGetGoodsQuery();
+  const [count, setCount] = useState('');
+  const [newProduct, setNewProduct] = useState('');
+  const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      await addProduct({ name: newProduct }).unwrap();//unwrap need to work with isError, isLoading etc
+      setNewProduct('');
+    }
+  }
+
+  const handleDeleteProduct = async (id) => {
+    await deleteProduct(id).unwrap();
+  }
 
   if (isLoading) return <h2>Loading...</h2>
 
   return (
     <div className="App">
+      <div style={{ display: 'flex' }}>
+        <input type="text" value={newProduct} onChange={(e) => setNewProduct(e.target.value)} />
+        <button onClick={handleAddProduct}>Add product</button>
+      </div>
+      <div>
+        <select value={count} onChange={(e) => setCount(e.target.value)}>
+          <option value="''">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
       <ul>
-        {data.map(item => <li key={item.id}>{item.name}</li>)}
+        {data.map(item => <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>{item.name}</li>)}
       </ul>
     </div>
   );
--- src/redux/goodsApi.js
+++ src/redux/goodsApi.js
@@ -2,12 +2,36 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const goodsApi = createApi({
     reducerPath: 'goodsApi',
+    tagTypes: ['Products'],
     baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
     endpoints: (build) => ({
         getGoods: build.query({
-            query: () => '/goods',
-        })
+            query: (limit = '') => `/goods?${limit && `_limit=${limit}`}`,
+            providesTags: (result, error, arg) =>
+                result
+                    ? [
+                        ...result.map(({ id }) => ({ type: 'Products', id })),
+                        { type: 'Products', id: 'List' },
+                    ]
+                    : [{ type: 'Products', id: 'List' }],
+        }),
+        addProduct: build.mutation({
+            query: (body) => ({
+                url: 'goods',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{ type: 'Products', id: 'List' }],
+        }),
+        deleteProduct: build.mutation({
+            query: (id) => ({
+                url: `goods/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{ type: 'Products', id: 'List' }],
+        }),
+
     })
 });
-
-export const { useGetGoodsQuery } = goodsApi; //хук создается автоматически берем use + getGoods + query
+//limit = '' есть из коробки в json-server;
+export const { useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation } = goodsApi; //хук создается автоматически берем use + getGoods + query
