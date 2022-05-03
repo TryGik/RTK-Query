d1344a765b027d4ec03e38df8b77cd9be7f86296
TryGik <ahutinon@gmail.com>
2022-05-03 14:08:44 +0300

    basic func

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { useGetGoodsQuery } from './redux';
 
 function App() {
+  const [count, setCount] = useState();
+  const { data = [], isLoading } = useGetGoodsQuery();
+
+  if (isLoading) return <h2>Loading...</h2>
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {data.map(item => <li key={item.id}>{item.name}</li>)}
+      </ul>
     </div>
   );
 }
---
+++ src/redux/index.js
@@ -0,0 +1,4 @@
+export * from './store';
+export * from './goodsApi';
+
+//* - it means that export all from...
